4855049ab64fd4f0ccd899bc7872bcadfa79d3e1
zFernand0 <fernando.rijocedeno@broadcom.com>
2022-03-24 11:43:19 +0000

    configure eslint and add license
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  "plugins": [
+    "@typescript-eslint",
+    "license-header",
+    "unused-imports",
+  ],
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "rules": {
+    "license-header/header": ["error", "LICENSE_HEADER"],
+    "max-len": ["warn", 150],
+    // "no-console": "error",
+    "no-multiple-empty-lines": "warn",
+    "no-trailing-spaces": "warn",
+    "@typescript-eslint/ban-types": "off",
+    // "@typescript-eslint/comma-dangle": ["warn", "only-multiline"],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/indent": ["warn", 2],
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/no-magic-numbers": "off",
+    // "@typescript-eslint/no-magic-numbers": ["warn", {
+    //     "ignore": [-1, 0, 1, 2],
+    //     "ignoreDefaultValues": true,
+    //     "ignoreReadonlyClassProperties": true
+    // }],
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/semi": "warn",
+    "unused-imports/no-unused-imports": "warn",
+    "unused-imports/no-unused-vars": "warn",
+    // "unused-imports/no-unused-vars": ["warn", { "args": "none" }]
+  }
+};
