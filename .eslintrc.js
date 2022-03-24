1a6a1a69c06ef0c052596f21e40999ce6177bea7
zFernand0 <fernando.rijocedeno@broadcom.com>
2022-03-24 11:59:46 +0000

    update parser
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     "license-header",
     "unused-imports",
   ],
+  "parser": "@typescript-eslint/parser",
   "parserOptions": {
     "ecmaVersion": 12,
     "sourceType": "module"
--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,31 @@
  * For a copy, see <https://opensource.org/licenses/MIT>.
 **/
 
+import * as path from "path";
+import * as vscode from "vscode";
+
+import { getZoweDir } from "@zowe/core-for-zowe-sdk";
+import { IProfAttrs, ProfileInfo } from "@zowe/imperative";
+
 export async function activate() {
   console.log('Congratulations, your extension "vsce" is now active!');
+
+  const profInfo = new ProfileInfo("zowe");
+  // determine profile location
+  // The default zowe home folder or a project level workspace
+  let rootPath: string;
+  if (vscode.workspace.workspaceFolders) {
+    rootPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+    await profInfo.readProfilesFromDisk({ projectDir: path.normalize(rootPath) });  // homeDir is used even if not present
+  } else {
+    await profInfo.readProfilesFromDisk({ homeDir: getZoweDir() });   // looks like homeDir can be excluded as it is used by default
+  }
+  if (!profInfo.usingTeamConfig) {
+    vscode.window.showErrorMessage(`No zowe V2 Teams profile found. Please ensure that a V2 profile exists`);
+  }
+  const typeProfiles: IProfAttrs[] = profInfo.getAllProfiles("zosmf");
+  const baseProfiles: IProfAttrs[] = profInfo.getAllProfiles("base");
+
+  console.log("--- typeProfiles", typeProfiles);
+  console.log("--- baseProfiles", baseProfiles);
 }
