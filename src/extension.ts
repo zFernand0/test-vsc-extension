c11e43b1b53260e603cbaf292e524fe9a73eb8b1
zFernand0 <fernando.rijocedeno@broadcom.com>
2022-03-24 11:47:00 +0000

    add test-keytar stuff
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,21 @@
  * For a copy, see <https://opensource.org/licenses/MIT>.
 **/
 
+import { CredentialManagerFactory, Logger, ProfileInfo } from '@zowe/imperative';
+import { KeytarApi, KeytarCredentialManager } from "@zowe/zowe-explorer-api";
+
 export async function activate() {
-  console.log('Congratulations, your extension "vsce" is now active!');
+	console.log('Congratulations, your extension "vsce" is now active!');
+	const log = Logger.getAppLogger();
+
+	const keytarApi = new KeytarApi(log);
+	console.log("keytarApi", keytarApi);
+	await keytarApi.activateKeytar(CredentialManagerFactory.initialized,false);
+	console.log("keytar", KeytarCredentialManager.keytar);
+	try {
+		const test = new ProfileInfo("zowe");
+		console.log(test)
+	} catch (e) {
+		console.log(e);
+	}
 }
